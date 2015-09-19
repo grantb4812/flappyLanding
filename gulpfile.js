a129c854dafb930be95376cc6763881f03f2a824
grantb4812 <grantb4812@gmail.com>
2015-09-18 19:58:22 -0500

    working on landing page cant get live reload to work

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 
+var livereload = require('gulp-livereload');
 var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
 var imagemin = require('gulp-imagemin');
@@ -54,8 +55,18 @@ gulp.task('images', function() {
     .pipe(gulp.dest('build/img'));
 });
 
+//adding live reload task
+/*
+gulp.task('reload', function(){
+  return gulp.src('site/scss/*.scss')
+      .pipe(sass())
+      .pipe(gulp.dest('site/css'))
+      .pipe(livereload());
+});
+*/
 // Watch task
 gulp.task('watch', function() {
+  // livereload.listen();
   gulp.watch('site/js/*.js', ['jshint']);
   gulp.watch('site/scss/*.scss', ['sass']);
   gulp.watch('site/css/*.css', ['styles']);
